17d66cb558e1dd4aa430cc4d98eab917ae24be2e
isis <idiazr@miumg.edu.gt>
2020-06-03 11:16:15 -0600

    MDI

---
+++ Examen/Capa_Datos/Sentencias.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Datos
+{
+   public class Sentencias
+    {
+        Conexion cn = new Conexion();
+        OdbcCommand comm;
+
+        public OdbcDataReader insertarbitacora(string sCodigo, string sip, string Smac, string susuario, string sdepartamento, string sfechahora, string saccion, string sformulario)
+        {
+            try
+            {
+                cn.conexionbd();
+                string consulta = "insert into bitacora values(" + sCodigo + ", '" + sip + "', '" + Smac + "' ,'" + susuario + "','" + sdepartamento + "','" + sfechahora + "','" + saccion + "','" + sformulario + "');";
+                comm = new OdbcCommand(consulta, cn.conexionbd());
+                OdbcDataReader mostrar = comm.ExecuteReader();
+                return mostrar;
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+                return null;
+            }
+        }
+
+        public string obtenerfinal(string tabla, string campo)
+        {
+            String camporesultante = "";
+            try
+            {
+                string sql = "SELECT MAX(" + campo + "+1) FROM " + tabla + ";"; //SELECT MAX(idFuncion) FROM `funciones`     
+                OdbcCommand command = new OdbcCommand(sql, cn.conexionbd());
+                OdbcDataReader reader = command.ExecuteReader();
+                reader.Read();
+                camporesultante = reader.GetValue(0).ToString();
+                Console.WriteLine("El resultado es: " + camporesultante);
+                if (String.IsNullOrEmpty(camporesultante))
+                    camporesultante = "1";
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(camporesultante);
+            }
+            return camporesultante;
+        }
+
+        public OdbcDataReader consultaayudas(string id)
+        {
+            try
+            {
+                cn.conexionbd();
+                string consultaGraAsis = " select * from ayudas where pkcodigoayuda =" + id + ";";
+                comm = new OdbcCommand(consultaGraAsis, cn.conexionbd());
+                OdbcDataReader mostrarResultados = comm.ExecuteReader();
+                return mostrarResultados;
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+                return null;
+            }
+        }
+
+//----------------------------------------------Sentencias--------------------------------------------------------------------------------------------------------------------------------------------------------------------------- 
+
+        public OdbcDataReader Insertar(string pkidProveedor, string Nombre, string Nit, string Direccion, string Telefono, string Email, string estado)
+        {
+
+            try
+            {
+                cn.conexionbd();
+                string consulta = "insert into proveedor values(" + pkidProveedor + ", '" + Nombre + "','" + Nit + "','" + Direccion + "','" + Telefono + "','" + Email + "','" + estado + "');";
+                comm = new OdbcCommand(consulta, cn.conexionbd());
+                OdbcDataReader mostrar = comm.ExecuteReader();
+                return mostrar;
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+                return null;
+            }
+        }
+
+        public OdbcDataReader modificar(string pkidProveedor, string Nombre, string Nit, string Direccion, string Telefono, string Email, string estado)
+        {
+            try
+            {
+                cn.conexionbd();
+                string consulta = "UPDATE proveedor set Nombre='" + Nombre + "',Nit='" + Nit + "',Direccion='" + Direccion + "',Telefono='" + Telefono + "',Email='" + Email + "',estado='" + estado + "' where pkidproveedor='" + pkidProveedor + "';";
+                comm = new OdbcCommand(consulta, cn.conexionbd());
+                OdbcDataReader mostrar = comm.ExecuteReader();
+                return mostrar;
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+                return null;
+            }
+        }
+
+        public OdbcDataReader eliminar(string pkidProveedor)
+        {
+            try
+            {
+                cn.conexionbd();
+                string consulta = "UPDATE proveedor set estado='0' where pkidProveedor='" + pkidProveedor + "';";
+                comm = new OdbcCommand(consulta, cn.conexionbd());
+                OdbcDataReader mostrar = comm.ExecuteReader();
+                return mostrar;
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+                return null;
+            }
+        }
+
+        public OdbcDataReader consulta()
+        {
+            try
+            {
+                cn.conexionbd();
+                string consulta = "SELECT * FROM proveedor WHERE estado = 1 ;";
+                comm = new OdbcCommand(consulta, cn.conexionbd());
+                OdbcDataReader mostrar = comm.ExecuteReader();
+                return mostrar;
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+                return null;
+            }
+
+        }
+
+    }
+}
